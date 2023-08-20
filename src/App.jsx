bf8351c943afc7b9d0ad0bfd322d3a96e525dc12
Shinkarenko Anton <AVEINDOMINUSNOX@yandex.ru>
2023-08-20 15:48:52 +0500

    Create architecture/Add Header

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Route, Routes } from 'react-router-dom';
+
+import AboutProject from './components/Main/AboutProject/AboutProject';
+import Header from './components/Common/Header/Header';
+import Login from './components/Auth/Login/Login';
+import Main from './components/Main/Main';
+import Movies from './components/Movies/Movies';
+import Profile from './components/Auth/Profile/Profile';
+import Register from './components/Auth/Register/Register';
+import SavedMovies from './components/SavedMovies/SavedMovies';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/about-project" element={<AboutProject />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/saved-movies" element={<SavedMovies />} />
+        <Route path="/signup" element={<Register />} />
+        <Route path="/signin" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
